9784fde49429a2e341d65c6213e96d33e109ff9d
liaozhipu <liaozhipu@sina.com>
2016-04-12 22:54:37 +0800

    +增加自动刷新页面任务

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,24 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');//文件合并
 var uglify = require('gulp-uglify');//js文件压缩
-var livereload=require("gulp-livereload");//刷新
+var livereload = require("gulp-livereload");//刷新
 
-gulp.task("default",function(){
-   return gulp.src("src/js/**.**")
-       .pipe(concat("app.min.js"))
-       .pipe(uglify())
-       .pipe(gulp.dest("dist/js"))
-       .pipe(livereload());
+gulp.task("default", function () {
+    return gulp.src(["src/js/**.**", "demo.js", "demo.html", "demo.css"])
+        .pipe(concat("app.min.js"))
+        .pipe(uglify())
+        .pipe(gulp.dest("dist/js"))
+        .pipe(livereload());
 });
-gulp.task("watch",function(){
-   livereload.listen();
-   gulp.watch("src/js/**.**",["default"]);
+gulp.task("demo", function () {
+    return gulp.src(["demo.js", "demo.html", "demo.css"])
+        .pipe(livereload());
+});
+gulp.task("watch", function () {
+    livereload.listen();
+    gulp.watch("src/js/**.**", ["default"]);
+});
+gulp.task("watchDemo", function () {
+    livereload.listen();
+    gulp.watch(["demo.js", "demo.css", "demo.html"], ["demo"]);
 });
