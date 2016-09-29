fd369a4e8ec4c59e1420dd2e47111953e11c7b4e
thyhates <thyhates@gmail.com>
2016-09-29 17:55:26 +0800

    new branch

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ var app = express();
 var fs = require("fs");
 var session = require("express-session");
 var crypto = require("crypto");
+const mongoClient = require("mongodb").MongoClient;
+const mongo = require("mongodb");
 app.use(express.static(__dirname + "/node_modules"));
 app.use(express.static(__dirname + "/src"));
 app.use(express.static(__dirname + "/dist"));
@@ -18,7 +20,7 @@ app.use(body_parser.urlencoded({extended: true}));
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
-app.get("/favicon.ico",function(req,res){
+app.get("/favicon.ico", function (req, res) {
     res.sendFile(__dirname + "/favicon.ico");
 });
 /*fs.readFile("doc/document.json", function (err, data) {
@@ -32,64 +34,118 @@ function getAllFiles() {
     var file = fs.readdirSync("doc");
     return file;
 }
-
+const mongoUrl = "mongodb://127.0.0.1:27017/wiki";
+const mongoUser = "thyhates";
+const mongoPwd = "123";
+let dbopt = {
+    find: function (co, query, callback) {
+        mongoClient.connect(mongoUrl, function (err, db) {
+            assert.equal(null, err);
+            db.authenticate(mongoUser, mongoPwd, function (err, result) {
+                assert.equal(true, result);
+                let collection = db.collection(co);
+                collection.find(query).toArray().then(function (docs) {
+                    assert.equal(null, err);
+                    db.close();
+                    callback(docs);
+                });
+            });
+        });
+    },
+    insert: function (co, query, callback) {
+        mongoClient.connect(mongoUrl, function (err, db) {
+            assert.equal(null, err);
+            db.authenticate(mongoUser, mongoPwd, function (err, result) {
+                assert.equal(true, result);
+                let collection = db.collection(co);
+                collection.insertOne(query).then(function (items) {
+                    assert.equal(1, items.insertedCount);
+                    db.close();
+                    callback(items);
+                });
+            });
+        });
+    },
+    update: function (co, query, newData, callback) {
+        mongoClient.connect(mongoUrl, function (err, db) {
+            assert.equal(null, err);
+            db.authenticate(mongoUser, mongoPwd, function (err, result) {
+                assert.equal(true, result);
+                let collection = db.collection(co);
+                collection.updateOne(query, newData).then(function (items) {
+                    assert.equal(1, items.matchedCount);
+                    db.close();
+                    callback(items);
+                });
+            });
+        });
+    },
+    delete: function (co, query, callback) {
+        mongoClient.connect(mongoUrl, function (err, db) {
+            assert.equal(null, err);
+            db.authenticate(mongoUser, mongoPwd, function (err, result) {
+                assert.equal(true, result);
+                let collection = db.collection(co);
+                collection.deleteOne(query).then(function (items) {
+                    db.close();
+                    callback(items);
+                });
+            });
+        });
+    }
+};
 var hs = crypto.createHash("md5").update("abcdefg").digest("hex");
 app.use(session({
     secret: hs,
-    saveUninitialized:true,
-    resave:false
+    saveUninitialized: true,
+    resave: false
 }));
-function addLog(action,target,user,apiName,apiIndex){
-    var logs=JSON.parse(fs.readFileSync("log.json"));
-    var time=new Date();
-    if(apiIndex===0){
-        apiIndex="0";
+function addLog(action, target, user, apiName, apiIndex) {
+    var logs = JSON.parse(fs.readFileSync("log.json"));
+    var time = new Date();
+    if (apiIndex === 0) {
+        apiIndex = "0";
     }
-    var log={
-        action:action,
-        time:time.toLocaleString(),
-        target:target,
-        user:user,
-        apiName:apiName||"",
-        apiIndex:apiIndex||""
+    var log = {
+        action: action,
+        time: time.toLocaleString(),
+        target: target,
+        user: user,
+        apiName: apiName || "",
+        apiIndex: apiIndex || ""
     };
-    if(logs.logs.length>30){
-        logs.logs.shift();
-    }
-    logs.logs.push(log);
     var errs;
-    fs.writeFile("log.json",JSON.stringify(logs),function(err){
-        if(err){
-            errs=err;
-        }else{
-            errs=false;
+    dbopt.insert("logs", log, function (result) {
+        if (result.result.ok === 1) {
+            errs = true;
+        } else {
+            errs = false;
         }
     });
     return errs;
 }
 app.post("/getAllDocs", function (req, res) {
-    var logined=true;
-    if(!isLogin(req)){
-        logined=false;
+    var logined = true;
+    if (!isLogin(req)) {
+        logined = false;
     }
-    var files = getAllFiles();
-    var body = [];
-    for (let i = 0; i < files.length; i++) {
-        var buffer = JSON.parse(fs.readFileSync("doc/" + files[i]));
-        body.push(buffer.docInfo);
-    }
-    res.status(200).send({status: true, model: body,logined:logined});
+    dbopt.find("docs", {}, function (result) {
+        res.status(200).send({
+            status: 1,
+            model: result
+        });
+    });
 });
 app.post("/getDocument", function (req, res) {
-    var name = req.body.name;
-    fs.readFile("doc/" + name + ".json", function (err, data) {
-        if (err) {
-            res.status(200).send({status: false, msg: "文档读取失败，或不存在"});
-        } else {
-            res.status(200).send({status: true, model: JSON.parse(data), msg: "文档读取成功"});
-        }
+    var query = {
+        doc_id: req.body.id
+    };
+    dbopt.find("apis", query, function (result) {
+        res.status(200).send({
+            status: 1,
+            model: result
+        });
     });
-
 });
 app.post("/newDocument", function (req, res) {
     if (!isLogin(req)) {
@@ -100,33 +156,24 @@ app.post("/newDocument", function (req, res) {
         type = req.body.type,
         description = req.body.description;
     var body = {
-        docInfo: {
-            name: name,
-            type: type,
-            description: description
-        },
-        apis: []
+        name: name,
+        type: type,
+        description: description
     };
-    fs.readFile("doc/" + name + ".json", function (err, data) {
-        if (err) {
-            fs.writeFile("doc/" + name + ".json", JSON.stringify(body), function (err) {
-                if (err) {
-                    res.status(400).send({
-                        status: false,
-                        msg: err
-                    });
-                } else {
-                    var errs=addLog("创建文档",name,req.session.username);
-                    if(!errs){
-                        res.status(200).send({status: true, msg: "文档创建成功"});
-                    }else{
-                        console.log(errs);
-                        res.status(200).send({status:false,msg:"添加操作日志失败",model:errs})
-                    }
-                }
-            });
+    dbopt.insert("docs", body, function (result) {
+        if (result.result.ok === 1) {
+            var errs = addLog("创建文档", name, req.session.username);
+            if (!errs) {
+                res.status(200).send({status: true, msg: "文档创建成功"});
+            } else {
+                console.log(errs);
+                res.status(200).send({status: false, msg: "添加操作日志失败", model: errs})
+            }
         } else {
-            res.status(200).send({status: false, msg: "该文档已存在"});
+            res.status(200).send({
+                status: SQLERROR,
+                msg: "未知错误，请重试", model: result
+            });
         }
     });
 });
@@ -135,124 +182,118 @@ app.post("/editDocs", function (req, res) {
         res.status(401).send({msg: "请先登录"});
         return false;
     }
-    var name = req.body.name;
-    var info = JSON.parse(fs.readFileSync("doc/" + name + ".json"));
+    var doc_id = req.body.doc_id;
     var newInfo = req.body.info;
-    if (name != newInfo.name) {
-        fs.unlink("doc/" + info.docInfo.name + ".json", function (err) {
-            if (err) {
-                res.status(200).send({status: false, msg: err});
-                return false;
-            }
+    var query = {
+        _id: new mongo.ObjectId(id)
+    };
+    var newData = {$set: newInfo};
+    dbopt.update("docs", query, newData, function (result) {
+        res.status(200).send({
+            status: true,
+            model: result,
+            msg: "修改成功"
         });
-    }
-    info.docInfo = newInfo;
-    fs.writeFile("doc/" + newInfo.name + ".json", JSON.stringify(info), function (err) {
-        if (err) {
-            res.status(200).send({status: false, msg: "修改失败。"})
-        } else {
-            var errs=addLog("修改文档",newInfo.name,req.session.username);
-            if(!errs){
-                res.status(200).send({status: true, msg: "修改成功"});
-            }else{
-                console.log(errs);
-                res.status(200).send({status:false,msg:"添加操作日志失败",model:errs})
-            }
-        }
     });
-
 });
 app.post("/delDocument", function (req, res) {
     if (!isLogin(req)) {
         res.status(401).send({msg: "请先登录"});
         return false;
     }
-    var name = req.body.name;
-    fs.unlink("doc/" + name + ".json", function (err) {
-        if (err) {
-            res.status(200).send({status: false, msg: err});
-        } else {
-            var errs=addLog("删除文档",name,req.session.username);
-            if(!errs){
-                res.status(200).send({status: true, msg: "文档删除成功"});
-            }else{
-                console.log(errs);
-                res.status(200).send({status:false,msg:"添加操作日志失败",model:errs})
-            }
+    var doc_id = req.body.doc_id;
+    var query = {
+        _id: new mongo.ObjectId(doc_id)
+    };
+    var apiQuery = {
+        doc_id: doc_id
+    };
+    dbopt.delete("docs", query, function (result) {
+        if (result.result.ok === 1) {
+            dbopt.delete("apis", apiQuery, function (results) {
+                if (result.result.ok === 1) {
+                    res.status(200).send({
+                        status: true,
+                        msg: "删除成功"
+                    });
+                } else {
+                    res.status(200).send({
+                        status: -1,
+                        msg: "服务器异常",
+                        model: result
+                    });
+                }
+            });
+
         }
     });
 });
-app.post("/editErrorCode",function(req,res){
+app.post("/editErrorCode", function (req, res) {
     if (!isLogin(req)) {
         res.status(401).send({msg: "请先登录"});
         return false;
     }
-    var name = req.body.name;
+    var doc_id = req.body.doc_id;
     var body = req.body.body;
-    var errInfo = JSON.parse(fs.readFileSync("doc/" + name + ".json"));
-    errInfo.errorCodeLst=body;
-    fs.writeFile("doc/" + name + ".json", JSON.stringify(errInfo), function (err) {
-        if (err) {
-            res.status(200).send({status: false, msg: "编辑失败"});
-        } else {
-            var errs=addLog("编辑错误码",name,req.session.username);
-            if(!errs){
-                res.status(200).send({status: true, msg: "编辑成功"});
-            }else{
-                res.status(200).send({status:false,msg:"添加操作日志失败",model:errs});
-            }
-        }
-    })
+
+    var errorCodeLst = body;
+    var query = {
+        _id: new mongo.ObjectId(doc_id)
+    };
+    var newData = {$set: {errorCodeLst: body}};
+    dbopt.update("docs", query, newData, function (result) {
+        res.status(200).send({
+            status: true,
+            model: result,
+            msg: "修改成功"
+        });
+    });
 });
 app.post("/addApi", function (req, res) {
     if (!isLogin(req)) {
         res.status(401).send({msg: "请先登录"});
         return false;
     }
-    var apiName = req.body.name;
     var api = req.body.body;
-    api.createUser=req.session.username;
-    api.createTime=new Date();
-    var json = JSON.parse(fs.readFileSync("doc/" + apiName + ".json"));
-    json.apis.push(api);
-    fs.writeFile("doc/" + apiName + ".json", JSON.stringify(json), function (err) {
-        if (err) {
-            res.status(200).send({status: false, msg: "Api添加失败"});
-        } else {
-            var errs=addLog("添加API",apiName,req.session.username,api.name,json.apis.length-1);
-            if(!errs){
+    api.createUser = req.session.username;
+    api.createTime = new Date();
+    dbopt.insert("apis", api, function (result) {
+        if (result.result.ok === 1) {
+            var errs = addLog("创建文档", api.name, req.session.username);
+            if (!errs) {
                 res.status(200).send({status: true, msg: "Api添加成功"});
-            }else{
-                res.status(200).send({status:false,msg:"添加操作日志失败",model:errs})
+            } else {
+                console.log(errs);
+                res.status(200).send({status: false, msg: "添加操作日志失败", model: errs})
             }
+        } else {
+            res.status(200).send({
+                status: -1,
+                msg: "Api添加失败", model: result
+            });
         }
-    })
+    });
 });
 app.post("/delApi", function (req, res) {
     if (!isLogin(req)) {
         res.status(401).send({msg: "请先登录"});
         return false;
     }
-    var name = req.body.name;
-    var index = req.body.index;
-    var body = fs.readFile("doc/" + name + ".json", function (err, data) {
-        if (err) {
-            res.status(200).send({status: false, msg: "api删除失败。"});
+    var id = req.body.id;
+    var query = {
+        _id: new mongo.ObjectId(id)
+    }
+    dbopt.delete("apis", query, function (results) {
+        if (result.result.ok === 1) {
+            res.status(200).send({
+                status: true,
+                msg: "删除成功"
+            });
         } else {
-            var info = JSON.parse(data);
-            var apiName=info.apis[index].name;
-            info.apis.splice(index, 1);
-            fs.writeFile("doc/" + name + ".json", JSON.stringify(info), function (err) {
-                if (err) {
-                    res.status(200).send({status: false, msg: "api写入失败"});
-                } else {
-                    var errs=addLog("删除API",name,req.session.username,apiName);
-                    if(!errs){
-                        res.status(200).send({status: true, msg: "api删除成功"});
-                    }else{
-                        res.status(200).send({status:false,msg:"添加操作日志失败",model:errs})
-                    }
-                }
+            res.status(200).send({
+                status: -1,
+                msg: "服务器异常",
+                model: result
             });
         }
     });
@@ -262,34 +303,30 @@ app.post("/editApi", function (req, res) {
         res.status(401).send({msg: "请先登录"});
         return false;
     }
-    var name = req.body.name;
+    var name = req.body.id;
     var newInfo = req.body.api;
-    newInfo.update=req.session.username;
-    newInfo.lastTime=new Date();
-    var index = req.body.index;
-    var apiInfo = JSON.parse(fs.readFileSync("doc/" + name + ".json"));
-    apiInfo.apis[index] = newInfo;
-    fs.writeFile("doc/" + name + ".json", JSON.stringify(apiInfo), function (err) {
-        if (err) {
-            res.status(200).send({status: false, msg: "编辑失败"});
-        } else {
-            var errs=addLog("编辑API",name,req.session.username,newInfo.name,index);
-            if(!errs){
-                res.status(200).send({status: true, msg: "编辑成功"});
-            }else{
-                res.status(200).send({status:false,msg:"添加操作日志失败",model:errs})
-            }
-        }
-    })
+    newInfo.update = req.session.username;
+    newInfo.lastTime = new Date();
+    var query = {
+        _id: new mongo.ObjectId(id)
+    };
+    var newData = {$set: newInfo};
+    dbopt.update("docs", query, newData, function (result) {
+        res.status(200).send({
+            status: true,
+            model: result,
+            msg: "修改成功"
+        });
+    });
 });
-app.post("/getLog",function(req,res){
-    fs.readFile("log.json",function(err,data){
-       if(err){
-           console.log(err);
-           res.status(200).send({status:false,msg:"日志读取失败"});
-       } else{
-           res.status(200).send({status:true,msg:"日志读取成功",model:JSON.parse(data)});
-       }
+app.post("/getLog", function (req, res) {
+    fs.readFile("log.json", function (err, data) {
+        if (err) {
+            console.log(err);
+            res.status(200).send({status: false, msg: "日志读取失败"});
+        } else {
+            res.status(200).send({status: true, msg: "日志读取成功", model: JSON.parse(data)});
+        }
     });
 });
 app.post("/login", function (req, res) {
@@ -305,7 +342,7 @@ app.post("/login", function (req, res) {
         });
         if (isRight) {
             req.session.isLogin = true;
-            req.session.username=reqName;
+            req.session.username = reqName;
             res.status(200).send({status: true, msg: "登录成功"});
         } else {
             res.status(401).send({status: false, msg: "用户名或密码不正确！"});
@@ -314,7 +351,7 @@ app.post("/login", function (req, res) {
 });
 app.post("/logout", function (req, res) {
     req.session.isLogin = false;
-    req.session.username=null;
+    req.session.username = null;
     console.log(req.session.isLogin);
     res.status(200).send({msg: "退出成功"});
 });
@@ -324,10 +361,11 @@ function isLogin(req) {
     }
     return true;
 }
+
 app.use(function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
-app.listen(8084, function () {
+app.listen(8086, function () {
     console.log("It's express,welcome!  127.0.0.1:8084");
 });
 
