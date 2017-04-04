b7192dfc1943b133b9a60b4d712a3ef7c88af3ae
thyhates <thyhates@gmail.com>
2017-04-05 07:23:57 +0800

    add jwt

--- src/js/app.js
+++ src/js/app.js
@@ -4,14 +4,6 @@
 "use strict";
 var app = angular.module("app", ["angular-loading-bar", "ngAnimate",
     'ui.router', "toastr", "ui.bootstrap", "schemaForm", "angularBootstrapNavTree"])
-    .run(["$rootScope", "$state", "$timeout", "$location",
-        function ($rootScope, $state, $timeout, $location) {
+    .run(function () {
             console.log("app started...");
-            $rootScope.isLogin = sessionStorage.isLogin;
-            var neeToLogin = $location.search();
-            if (neeToLogin.tmethod === "login") {
-                $timeout(function () {
-                    $state.go("home.login");
-                });
-            }
-        }]);
+        });
